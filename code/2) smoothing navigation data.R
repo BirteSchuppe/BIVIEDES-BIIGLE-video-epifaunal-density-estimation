2fc6fed0535fa1de4ac01843e542095ae17843f5
BirteSchuppe <birte.k.schuppe@nord.no>
2025-03-25 11:40:47 +0100

    update birte

--- code/2) smoothing navigation data.R
+++ code/2) smoothing navigation data.R
@@ -4,20 +4,13 @@ library(tsibble)
 library(zoo)
 library(tidyverse)
 library(magrittr)
+library(stringr)
 
 # open the table with time and xy positions, use replace section to replace individual variabels faster
 setwd("D:/PHD/ROV/ROV raw video density estimates/navigation_smoothing")
-read_csv("20210107-093101 COMServer_matchedtovideo.csv") -> navigation
+read_csv("Generic_navigation.csv") -> navigation
 
-# select the relevant columns
-navigation %<>% select(Sperre_LOG_DATETIME, Sperre_Depth, Sperre_Heading, Survey_ROV_LON, Survey_ROV_LAT, Survey_LOG_RELTIME) 
-
-# make sure XY columns are numeric by removing |xb0 (for navigation files with symbols of degree and minutes)
-navigation$Survey_ROV_LON <- str_remove(navigation$Survey_ROV_LON, pattern = "\xb0")
-navigation$Survey_ROV_LAT <- str_remove(navigation$Survey_ROV_LAT, pattern = "\xb0")
-
-navigation %>% mutate(Survey_ROV_LON2 = as.numeric(Survey_ROV_LON), Survey_ROV_LAT2 =as.numeric(Survey_ROV_LAT)) -> navigation
-view(navigation) ### why is as numeric removing the last digits of the Latitude and longitude?
+ ### why is as numeric removing the last digits of the Latitude and longitude?
 # fill the gaps in the shp file
 # time managment
 navigation %>% 
@@ -28,7 +21,7 @@ navigation %>%
 
 
 # create the missing time stamps so that you have a reading at each second 
-maskxy %>% select(mtime, Survey_LOG_RELTIME,Survey_ROV_LON2,Survey_ROV_LAT2, Sperre_LOG_DATETIME, Sperre_Depth, Sperre_Heading) %>% 
+maskxy %>% select(mtime, Survey_LOG_RELTIME,Survey_ROV_LON,Survey_ROV_LAT, Sperre_LOG_DATETIME, Sperre_Depth, Sperre_Heading) %>% 
   as_tsibble(index = mtime) %>%
   fill_gaps() %>% # automatically creates the missing timestamps
   as_tibble() %>% 
@@ -38,8 +31,8 @@ maskxy %>% select(mtime, Survey_LOG_RELTIME,Survey_ROV_LON2,Survey_ROV_LAT2, Spe
   # interpolate the coordinates values for the timestamps you have created
   # these wont be accurate but you will replace them soon
   # they are still a basic interpolation of the closest coordinates 
-  mutate(LON2 = zoo::na.approx(as.vector(Survey_ROV_LON2)),
-         LAT2 = zoo::na.approx(as.vector(Survey_ROV_LAT2))) -> maskxy  
+  mutate(LON2 = zoo::na.approx(as.vector(Survey_ROV_LON)),
+         LAT2 = zoo::na.approx(as.vector(Survey_ROV_LAT))) -> maskxy  
 # note this might also be enough if your track loog good
 
 maskxy %>%  filter( is.na(LAT2) ) # there shouldn???t be any NA left - but check does no harm
